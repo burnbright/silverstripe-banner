7701ff51341004ac83f63bc47e1f9ea780391c55
Jeremy Shipman <jeremy@burnbright.net>
2014-10-09 13:25:27 +1300

    Allow content editors to edit banners.

--- code/BannerImage.php
+++ code/BannerImage.php
@@ -41,4 +41,20 @@ class BannerImage extends DataObject{
 		}
 	}
 
+	public function canCreate($member = null) {
+		return Permission::check("CMS_ACCESS_CMSMain");
+	}
+
+	public function canEdit($member = null) {
+		return Permission::check("CMS_ACCESS_CMSMain");
+	}
+
+	public function canDelete($member = null) {
+		return Permission::check("CMS_ACCESS_CMSMain");
+	}
+
+	public function canView($member = null) {
+		return Permission::check("CMS_ACCESS_CMSMain");
+	}
+
 }
