3373da08d41d3e4c4979e8672ed46dc2cb651244
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:49:23 +1300

    Converted to PSR-2

--- code/BannerImage.php
+++ code/BannerImage.php
@@ -1,60 +1,66 @@
 <?php
 
-class BannerImage extends DataObject{
+class BannerImage extends DataObject
+{
 
-	private static $db = array(
-		'Title' => 'Varchar(255)',
-		'SubTitle' => 'Varchar(255)',
-		'Link' => 'LinkField',
-		'Sort' => 'Int'
-	);
+    private static $db = array(
+        'Title' => 'Varchar(255)',
+        'SubTitle' => 'Varchar(255)',
+        'Link' => 'LinkField',
+        'Sort' => 'Int'
+    );
 
-	private static $has_one = array(
-		'Image' => 'Image',
-		'Parent' => 'Page'
-	);
+    private static $has_one = array(
+        'Image' => 'Image',
+        'Parent' => 'Page'
+    );
 
-	private static $summary_fields = array(
-		'Image.CMSThumbnail' => 'Image',
-		'CMSTitle' => 'Title',
-		'Link' => 'Link'
-	);
+    private static $summary_fields = array(
+        'Image.CMSThumbnail' => 'Image',
+        'CMSTitle' => 'Title',
+        'Link' => 'Link'
+    );
 
-	private static $default_sort = "\"Sort\" ASC, \"ID\" ASC";
+    private static $default_sort = "\"Sort\" ASC, \"ID\" ASC";
 
-	public function getCMSFields(){
-		$fields = parent::getCMSFields();
-		$image = $fields->fieldByName('Root.Main.Image');
-		$fields->removeByName('Image');
-		$fields->insertBefore($image,'Title');
-		$fields->removeByName('ParentID');
-		$fields->removeByName('Sort');
-		return $fields;
-	}
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $image = $fields->fieldByName('Root.Main.Image');
+        $fields->removeByName('Image');
+        $fields->insertBefore($image, 'Title');
+        $fields->removeByName('ParentID');
+        $fields->removeByName('Sort');
+        return $fields;
+    }
 
-	function getCMSTitle(){
-		if($this->Title){
-			return $this->Title;
-		}
-		if($this->Image()){
-			return $this->Image()->Title;
-		}
-	}
+    public function getCMSTitle()
+    {
+        if ($this->Title) {
+            return $this->Title;
+        }
+        if ($this->Image()) {
+            return $this->Image()->Title;
+        }
+    }
 
-	public function canCreate($member = null) {
-		return Permission::check("CMS_ACCESS_CMSMain");
-	}
+    public function canCreate($member = null)
+    {
+        return Permission::check("CMS_ACCESS_CMSMain");
+    }
 
-	public function canEdit($member = null) {
-		return Permission::check("CMS_ACCESS_CMSMain");
-	}
+    public function canEdit($member = null)
+    {
+        return Permission::check("CMS_ACCESS_CMSMain");
+    }
 
-	public function canDelete($member = null) {
-		return Permission::check("CMS_ACCESS_CMSMain");
-	}
+    public function canDelete($member = null)
+    {
+        return Permission::check("CMS_ACCESS_CMSMain");
+    }
 
-	public function canView($member = null) {
-		return Permission::check("CMS_ACCESS_CMSMain");
-	}
-
+    public function canView($member = null)
+    {
+        return Permission::check("CMS_ACCESS_CMSMain");
+    }
 }
--- code/BannersExtension.php
+++ code/BannersExtension.php
@@ -1,28 +1,31 @@
 <?php
 
-class BannersExtension extends DataExtension{
-	
-	static $has_many = array(
-		'Banners' => 'BannerImage'
-	);
+class BannersExtension extends DataExtension
+{
+    
+    public static $has_many = array(
+        'Banners' => 'BannerImage'
+    );
 
-	public function updateCMSFields(FieldList $fields){
-		$fields->addFieldsToTab("Root.Banners",
-			GridField::create("Banners",null,$this->owner->Banners(),
-				$config = GridFieldConfig_RecordEditor::create()
-			)
-		);
-		if(class_exists("GridFieldOrderableRows")){
-			$config->addComponent(new GridFieldOrderableRows());
-		}
-	}
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->addFieldsToTab("Root.Banners",
+            GridField::create("Banners", null, $this->owner->Banners(),
+                $config = GridFieldConfig_RecordEditor::create()
+            )
+        );
+        if (class_exists("GridFieldOrderableRows")) {
+            $config->addComponent(new GridFieldOrderableRows());
+        }
+    }
 
-	public function getFirstBanner(){
-		return $this->owner->Banners()->first();
-	}
+    public function getFirstBanner()
+    {
+        return $this->owner->Banners()->first();
+    }
 
-	public function getRandomBanner(){
-		return $this->owner->Banners()->sort("RAND()")->first();
-	}
-
+    public function getRandomBanner()
+    {
+        return $this->owner->Banners()->sort("RAND()")->first();
+    }
 }
